e242f95f2e61df1d844fae680e04cbf05f288aa1
Anton Novikov <an@dvhb.ru>
2016-01-14 16:25:48 +0300

    Update 2016-01-14T13:25:45.853Z

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    scopes: [
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'https://www.googleapis.com/auth/drive.install',
+        'https://www.googleapis.com/auth/drive.file',
+        'profile'
+    ],
+    clientId: '759755084920-e9ct1r1gcrp10od5q4ab9uclcarmjt68.apps.googleusercontent.com'
+}
